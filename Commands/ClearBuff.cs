90e1001f0afa416e490fed6c36531fa73d7c35b6
dotlake <dotlake@CHATTY-STICK>
2026-01-07 10:16:00 -0700

    Add clearbuff command

---
+++ Commands/ClearBuff.cs
@@ -0,0 +1,24 @@
+using MoreCommands.Common;
+using System;
+
+namespace CommandsExtended.Commands;
+
+public sealed class ClearBuff : TogglableCommandBase
+{
+    public override string[] Aliases => ["clearbuff", "clear"];
+
+    public override CommandTag Tag => CommandTag.Player;
+
+    public override string Description => "Clear any buffs";
+
+    public override bool CheatsOnly => true;
+
+    public override Action<string[]> GetLogicCallback()
+    {
+        return args =>
+        {
+            ENT_Player.playerObject?.curBuffs?.Initialize();
+        };
+    }
+}
+
