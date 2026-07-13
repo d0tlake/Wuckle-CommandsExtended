45a354b3d67066bce626139db683378030e4ecd6
dotlake <dotlake@CHATTY-STICK>
2026-07-12 23:09:23 -0600

    small stuff

--- Patches/ResolutionPatch.cs
+++ Patches/ResolutionPatch.cs
@@ -8,13 +8,12 @@ namespace CommandsExtended.Patches
     [HarmonyPatch(typeof(Screen))]
     public static class ResolutionPatch
     {
-        // Persistent config references bound from the main plugin initialization loop
         public static ConfigEntry<bool> Enabled;
         public static ConfigEntry<bool> CustomResActive;
         public static ConfigEntry<int> Width;
         public static ConfigEntry<int> Height;
 
-        [HarmonyPatch(nameof(Screen.SetResolution), new Type[] { typeof(int), typeof(int), typeof(FullScreenMode), typeof(RefreshRate) })]
+        [HarmonyPatch(nameof(Screen.SetResolution), [typeof(int), typeof(int), typeof(FullScreenMode), typeof(RefreshRate)])]
         [HarmonyPrefix]
         public static void Prefix(ref int width, ref int height, ref FullScreenMode fullscreenMode)
         {
